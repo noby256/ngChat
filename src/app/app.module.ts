16831c8bb33ad65aa5b11650fba00350aa86cc99
Valery Mikhnevich <noby256@gmail.com>
2021-05-14 00:23:16 +0200

    added socket.io module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { SocketIoModule } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot({ url: 'http://localhost:4444', options: {} })
   ],
   providers: [],
   bootstrap: [AppComponent]
